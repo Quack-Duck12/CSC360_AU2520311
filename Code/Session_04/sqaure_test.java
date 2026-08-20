a9b8627f42f48179930140c5bf81f1212f3fa7e3
Jevis <jevismaniyar111@gmail.com>
2026-08-20 15:31:02 +0530

    Implemented the Sqaure Code Explanation which is being used as example for graphics code

---
+++ Code/Session_04/sqaure_test.java
@@ -0,0 +1,23 @@
+package Session_04;
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class sqaure_test {
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame("Swing Square Outline"){
+            @Override
+            public void paint(Graphics g){
+                super.paint(g);
+                g.setColor(Color.ORANGE);
+
+                g.drawRect(100, 100, 150, 150);
+            }
+        };
+
+        frame.setSize(1280, 720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
